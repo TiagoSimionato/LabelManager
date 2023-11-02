6c3db6541002dc2182250b3e88dd41f65185b822
tiago-simionato <tiago.simionato5@gmail.com>
2023-11-02 17:36:11 -0300

    minor refactor + quality of life

--- src/labelManager.py
+++ src/labelManager.py
@@ -3,68 +3,69 @@ from os import listdir, remove, mkdir, path
 from webbrowser import open_new_tab
 from zipfile import ZipFile
 
+class Prefs:
+  removeZip     = None
+  openPdf       = None
+  zipFolderPath = None
+  outputPath    = None
+
 def toPdf():
-    for zipFile in listdir(zipFolderPath):
-        if zipFile.endswith(".zip") and zipFile.startswith("Etiqueta MercadoEnvios"):
-            with ZipFile(path.realpath(zipFolderPath) + '\\' + zipFile, 'r') as zip_ref:
-                with zip_ref.open("Etiqueta de envio.txt") as zpl:
-                    strZpl = ""
-                    for line in zpl:
-                        strZpl += str(line)
+  for zipFile in listdir(Prefs.zipFolderPath):
+    if zipFile.endswith(".zip") and zipFile.startswith("Etiqueta MercadoEnvios"):
+      with ZipFile(path.realpath(Prefs.zipFolderPath) + '\\' + zipFile, 'r') as zip_ref:
+        with zip_ref.open("Etiqueta de envio.txt") as zpl:
+          strZpl = ""
+          for line in zpl:
+            strZpl += str(line)
 
-            pdfLabels = zpl2_to_pdf(strZpl)
+      pdfLabels = zpl2_to_pdf(strZpl)
 
-            if (not path.exists(outputPath)):
-                mkdir(outputPath)
+      if (not path.exists(Prefs.outputPath)):
+        mkdir(Prefs.outputPath)
                 
-            indexOffset = len(listdir(outputPath))
-            for i, label in enumerate(pdfLabels):
-                labelPath = path.realpath(outputPath) + '\\label{}.pdf'.format(i + indexOffset)
-                with open(labelPath, 'wb') as fid:
-                    fid.write(label)
-                    if openPdf:
-                        open_new_tab('file://' + labelPath)
+      indexOffset = len(listdir(Prefs.outputPath))
+      for i, label in enumerate(pdfLabels):
+        labelPath = path.realpath(Prefs.outputPath) + '\\label{}.pdf'.format(i + indexOffset)
+        with open(labelPath, 'wb') as fid:
+          fid.write(label)
+          if Prefs.openPdf:
+            open_new_tab('file://' + labelPath)
 
-            if removeZip:
-                remove(path.realpath(zipFolderPath) + '\\' + zipFile)
+      if Prefs.removeZip:
+        remove(path.realpath(Prefs.zipFolderPath) + '\\' + zipFile)
 
 def getPrefs():
-    if (not path.exists('./prefs')):
-        mkdir('./prefs')
+  prefsPath = './prefs.txt'
+  if (not path.exists(prefsPath)):
+    with open(prefsPath, 'w') as prefs:
+      prefs.write("DeleteZip:True\n")
+      prefs.write("OpenPdf:True\n")
+      prefs.write("ZipPath:./downloads\n")
+      prefs.write("OutputPath:./pdfs\n")
 
-    prefsPath = './prefs/prefs.txt'
-    if (not path.exists(prefsPath)):
-            with open(prefsPath, 'w') as prefs:
-                prefs.write("DeleteZip:True\n")
-                prefs.write("OpenPdf:True\n")
-                prefs.write("ZipPath:./downloads\n")
-                prefs.write("OutputPath:./pdfs\n")
-
-    with open(prefsPath, 'r') as prefs:
-        global removeZip 
-        removeZip = prefs.readline()[10:] == "True\n"
-        global openPdf
-        openPdf = prefs.readline()[8:] == "True\n"
-        global zipFolderPath
-        zipFolderPath = prefs.readline()[8:][:-1]
-        global outputPath
-        outputPath = prefs.readline()[11:][:-1]
+  with open(prefsPath, 'r') as prefs:
+    Prefs.removeZip = prefs.readline()[10:] == "True\n"
+    Prefs.openPdf = prefs.readline()[8:] == "True\n"
+    Prefs.zipFolderPath = prefs.readline()[8:][:-1]
+    Prefs.outputPath = prefs.readline()[11:][:-1]
 
 def changeConfigs(parameter, choice):
-    with open('./prefs/prefs.txt', 'r') as prefs:
-        newLine = parameter + ":" + str(choice) + "\n"
-        replacementString = ""
-        for line in prefs:
-            if (parameter in line):
-                replacementString += newLine
-            else:
-                replacementString += line
+  with open('./prefs/prefs.txt', 'r') as prefs:
+    newLine = parameter + ":" + str(choice) + "\n"
+    replacementString = ""
+    for line in prefs:
+      if (parameter in line):
+        replacementString += newLine
+      else:
+        replacementString += line
 
-    with open('./prefs/prefs.txt', 'w') as prefs:
-        prefs.write(replacementString)
+  with open('./prefs/prefs.txt', 'w') as prefs:
+    prefs.write(replacementString)
 
-    #Atualizo as variaveis    
-    getPrefs()
+  #Atualizo as variaveis    
+  getPrefs()
 
 getPrefs()
-import lmGUI
+
+if(__name__ == '__main__'):
+  import lmGUI
--- src/lmGUI.py
+++ src/lmGUI.py
@@ -1,65 +1,52 @@
 from tkinter import *
 from tkinter import messagebox
-from labelManager import changeConfigs, toPdf, zipFolderPath, outputPath
+from tkinter.filedialog import askdirectory
+from labelManager import changeConfigs, toPdf, Prefs
 
 subwindowSize = "300x130"
 
 def todo():
-    messagebox.showwarning("TO DO", "AINDA NÃO FIZ ESSA PARTE")
+  messagebox.showwarning("TO DO", "AINDA NÃO FIZ ESSA PARTE")
+
+def newPathWindow(mainText, configName):
+  def setPath():
+    newPath = askdirectory(initialdir="")
+    if (newPath):
+      changeConfigs(configName, newPath)
+      newWindow.destroy()
+
+  newWindow = Toplevel(root)
+  newWindow.title("")
+  newWindow.minsize(300, 100)
+   
+  label = Label(newWindow, text=mainText)
+  label.pack(padx=30, pady=10)
+
+  submitButton = Button(newWindow, text="Definir nova pasta", command=setPath, padx=20, pady=5)
+  submitButton.pack(pady=10)
 
 def newZipWindow():
-    def setPath():
-        changeConfigs("ZipPath", entry.get())
-        newWindow.destroy()
-
-    newWindow = Toplevel(root)
-    newWindow.title("")
-    
-    label = Label(newWindow, text="Caminho atual:\n\n" + zipFolderPath + "\n\nDigite o novo caminho da pasta de downloads")
-    label.pack(padx=30, pady=20)
-
-    entry = Entry(newWindow)
-    entry.pack()
-
-    submitButton = Button(newWindow, text="OK", command=setPath, padx=20, pady=5)
-    submitButton.pack(pady=20)
-
-    entry.focus()
+  newPathWindow("Caminho atual da pasta de downloads:\n\n" + Prefs.zipFolderPath, "ZipPath")
 
 def newPdfWindow():
-    def setPath():
-        changeConfigs("OutputPath", entry.get())
-        newWindow.destroy()
-
-    newWindow = Toplevel(root)
-    newWindow.title("")
-    
-    label = Label(newWindow, text="Caminho atual:\n\n" + outputPath + "\n\nDigite o novo caminho da pasta de pdfs")
-    label.pack(padx=30, pady=20)
-
-    entry = Entry(newWindow)
-    entry.pack()
-
-    submitButton = Button(newWindow, text="OK", command=setPath, padx=20, pady=5)
-    submitButton.pack(pady=20)
-
-    entry.focus()
+  newPathWindow("Caminho atual onde são gerados os PDFs:\n\n" + Prefs.outputPath, "OutputPath")
 
 def setRmZip():
-    changeConfigs("DeleteZip", messagebox.askquestion("Escolha uma opção", "Deseja Excluir os arquivos zips depois de converter para pdf?") == 'yes')
+  changeConfigs("DeleteZip", messagebox.askquestion("Escolha uma opção", "Deseja Excluir os arquivos zips depois de converter para pdf?") == 'yes')
 
 def setOpPdf():
-    changeConfigs("OpenPdf", messagebox.askquestion("Escolha uma opção", "Deseja abrir automaticamente os pdfs convertidos?") == 'yes')
+  changeConfigs("OpenPdf", messagebox.askquestion("Escolha uma opção", "Deseja abrir automaticamente os pdfs convertidos?") == 'yes')
 
 root = Tk()
 root.title("Label Manager")
 root.iconbitmap(default='./favicon/favicon.ico')
+root.minsize(302, 167)
 
 #Configurando o menu no topo da GUI
 mainMenu = Menu(root)
 configMenu = Menu(mainMenu, tearoff=0)
-configMenu.add_command(label="Definir Pasta dos Zips", command=newZipWindow)
-configMenu.add_command(label="Definir Pasta dos Pdfs", command=newPdfWindow)
+configMenu.add_command(label="Pasta dos Zips", command=newZipWindow)
+configMenu.add_command(label="Pasta dos PDFs", command=newPdfWindow)
 mainMenu.add_cascade(label="Configurações", menu=configMenu)
 prefMenu = Menu(mainMenu, tearoff=0)
 prefMenu.add_command(label="Excluir zips?", command=setRmZip)
